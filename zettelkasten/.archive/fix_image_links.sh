1f49d80539ff125b9e99d051533c2019d522a19d
eliot <eliotkh12@gmail.com>
2023-10-22 13:16:41 -0700

    new zettels, modified html and css files

--- zettelkasten/.archive/fix_image_links.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-TARGETDIR="../resources/zettel-images"
-for html_file in $(ls | grep .html); do
-    sed -i "s#<img src=\"./\(.*\).png\" alt=\"image\" />#<img src=\"$TARGETDIR/${html_file%.html}/\1.png\" alt=\"image\" \>#g" ./$html_file
-done
